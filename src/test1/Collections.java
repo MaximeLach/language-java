be722ca00853401ef088f7457f70b3cdc4ff7217
MaximeLach <maxime.lach@gmail.com>
2016-09-15 16:56:31 +0200

    3ème version : ajout des listes

---
+++ src/test1/Collections.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class Collections {
+
+    public static void main(String[] args) {
+//        ArrayList<String> liste = new ArrayList();
+//        liste.add("Coucou");
+//        liste.add("Hello");
+//         liste.add("Coucou");
+//         
+//        for (String s : liste) {
+//            System.out.println(s);
+//        }
+//        System.out.println(liste + " " + liste.size());
+//        System.out.println("Coucou : "+ liste.indexOf("Coucou"));
+//        
+//        liste.remove("Coucou");
+//        System.out.println(liste + " " + liste.size());
+//        System.out.println("Coucou : "+ liste.indexOf("Coucou"));
+
+        ArrayList<Integer> liste1 = new ArrayList();
+        for (int indice = 10; indice <= 100; indice = indice + 10) {
+            liste1.add(indice);
+        }
+
+        ArrayList<Integer> liste2 = new ArrayList();
+        for (int indice = 2; indice <= 100; indice = indice + 2) {
+            liste2.add(indice);
+        }
+
+        ArrayList<Integer> liste3 = new ArrayList();
+        for (int indice = 3; indice <= 99; indice = indice + 3) {
+            liste3.add(indice);
+        }
+
+        liste2.removeAll(liste1);
+        liste2.removeAll(liste3);
+
+        System.out.println(liste2);
+        System.out.println("Nombre : " + liste2.size());
+    }
+}
